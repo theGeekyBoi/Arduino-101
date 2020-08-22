c0457956d1e1c6675ef6fd6eb16d6214e4be5bf1
Aaditya Sharma <coursesbyaaditya@gmail.com>
2020-08-22 10:37:44 +0530

    Arduino 101
    Week 1

--- blink.ino
+++ blink.ino
@@ -1,12 +1,1 @@
-int led = 8; //initialising the variable for LED
-void setup() {
-  pinMode(led, OUTPUT);
-}
 
-void loop() {
-  digitalWrite(led, HIGH); //LED is on
-  delay(1000); //the number you put in is in milliseconds
-  digitalWrite(led, LOW); //LED is off
-  delay(1000);
-  // This code will make an LED blink on and off every second, ake sure to connect the pins to the legs of the LED's properly
-}
